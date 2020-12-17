0c347ecd96493acb728a822773447d7cee3fc5ae
judeadams21 <815386@mystma.org>
2020-12-17 17:59:40 +0000

    Top Five Movies Ver. I

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-#WRITE YOUR CODE IN THIS FILE
+def topFiveMovies(a, b, c, d, e):
+    return a + " " + b + " " + c + " " + d + " " + e 
+
+print (topFiveMovies("Synecdoche, NY", "Blade Runner", "The Shining", "Eraserhead", "Pulp Fiction"))
